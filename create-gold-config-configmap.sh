2a30c06628a3707c8f9986aca50048ec093c64bb
Joe Gregorio <joe@bitworking.org>
2025-11-04 17:45:58 -0500

    Fix configmap for gold configs.

--- create-gold-config-configmap.sh
+++ create-gold-config-configmap.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-k3s kubectl create configmap gold-goldmine-config --from-file=gold-config/ --dry-run=client -o yaml | k3s kubectl apply -f -
+k3s kubectl create configmap gold-goldmine-config --from-file=gold/gold-config/ --dry-run=client -o yaml | k3s kubectl apply -f -
